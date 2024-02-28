0195e028621cef349a4f08cf6726e2c92a747424
Dalton Riehl <dtonriehl@gmail.com>
2024-02-27 20:18:20 -0600

    Tasklister Mini Project Complete: Achievement Get!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    addTask(e.target["new-task-description"].value);
+  })
+})
+
+function addTask(task){
+  let p = document.createElement('p');
+  let deleteButton = document.createElement('button');
+  deleteButton.addEventListener('click', () => {
+    deleteButton.parentNode.remove();
+  })
+  deleteButton.textContent = "Delete";
+  p.textContent = `${task}  `;
+  document.querySelector('#tasks').appendChild(p);
+  p.appendChild(deleteButton);
+}
